b5fba3b41d5b3cffc9167981834c43b567f416b0
Prakash <prkjha25@gmail.com>
2020-07-26 17:49:22 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname,'public')));
+
+const botName = 'ChatCord Bot';
+
+io.on('connection',socket => {
+    //console.log('New WS connection...');
+
+    socket.emit('message',formatMessage(botName,'welcome to chat cord!'));
+
+    socket.broadcast.emit('message', formatMessage(botName,'A user has joined the chat'));
+
+    socket.on('disconnect',() => {
+        io.emit('message',formatMessage(botName,'a user has left the chat'));
+    });
+
+    socket.on('chatMessage',(msg) => {
+        io.emit('message',formatMessage('USER',msg));
+    });
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT,() => console.log(`Server running on  port ${PORT}`));
